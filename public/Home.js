596a14e079217c1b3ad85feb8fe7cdf8a5850e74
jasonmc02gap <jmelendez@growthaccelerationpartners.com>
2017-11-14 23:38:29 -0600

    first commit

---
+++ public/Home.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <div>
+            <h1>Hello World from rails</h1>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
